3c161f3f610c660f7106286b2d0f5f52f6d6b1bd
Anton Nahorny <antoha.nagorniy@gmail.com>
2024-08-15 00:01:54 +0300

    feat: implemented the function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    const nToStr = n.toString();
+    let result = '';
+    for (let i = nToStr.length - 1; i >= 0; i -= 1) {
+      result = result + nToStr[i];
+    } 
+    return parseInt(result);
 }
